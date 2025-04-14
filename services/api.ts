c8a6ec7298d0bae04d4e732b925d7c2387b222e4
Lwant <jaimain671@gmail.com>
2025-04-14 13:45:09 +0700

    finished the app

--- services/api.ts
+++ services/api.ts
@@ -23,3 +23,22 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   const data = await res.json();
   return data.results;
 };
+
+export const fetchMovieDetail = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const res = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      { method: "GET", headers: TMDB_CONFIG.HEADER }
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch movie detail!");
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
